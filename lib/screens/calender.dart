51ff1345f318a968b7eb7fa809bcbe8fa848ffd3
Ruddy <rudranilshil@gmail.com>
2024-07-25 09:02:10 +0530

    calender added

--- lib/screens/calender.dart
+++ lib/screens/calender.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'package:table_calendar/table_calendar.dart';
 
 class CalendarScreen extends StatefulWidget {
   final String authKey;
@@ -13,23 +14,7 @@ class CalendarScreen extends StatefulWidget {
 
 class _CalendarScreenState extends State<CalendarScreen> {
   final TextEditingController roomNumberController = TextEditingController();
-  final TextEditingController checkInDateController = TextEditingController();
-  final TextEditingController checkOutDateController = TextEditingController();
-
-  Future<void> _selectDate(
-      BuildContext context, TextEditingController controller) async {
-    DateTime? selectedDate = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2100),
-    );
-    if (selectedDate != null) {
-      setState(() {
-        controller.text = "${selectedDate.toLocal()}".split(' ')[0];
-      });
-    }
-  }
+  Map<DateTime, List> _bookedDates = {};
 
   Future<void> _checkAvailability() async {
     final response = await http.get(
@@ -43,17 +28,51 @@ class _CalendarScreenState extends State<CalendarScreen> {
     if (response.statusCode == 200) {
       final List<dynamic> roomData = jsonDecode(response.body);
 
-      // Check the room availability based on the response
       bool isAvailable = false;
 
       for (var room in roomData) {
         if (room['room_number'] == int.parse(roomNumberController.text)) {
           isAvailable = room['booked'] == 0;
+
+          if (!isAvailable) {
+            final bookedDatesResponse = await http.get(
+              Uri.parse(
+                  'http://localhost:3000/booked_dates?room_id=${room['id']}'),
+              headers: <String, String>{
+                'Content-Type': 'application/json; charset=UTF-8',
+                'Authorization': 'Bearer ${widget.authKey}',
+              },
+            );
+
+            if (bookedDatesResponse.statusCode == 200) {
+              final List<dynamic> bookedDatesData =
+                  jsonDecode(bookedDatesResponse.body);
+
+              _bookedDates.clear();
+              for (var date in bookedDatesData) {
+                DateTime checkInDate = DateTime.parse(date['check_in_date']);
+                DateTime checkOutDate = DateTime.parse(date['check_out_date']);
+                for (DateTime d = checkInDate;
+                    d.isBefore(checkOutDate) ||
+                        d.isAtSameMomentAs(checkOutDate);
+                    d = d.add(Duration(days: 1))) {
+                  if (_bookedDates[d] == null) {
+                    _bookedDates[d] = ['booked'];
+                  } else {
+                    _bookedDates[d]?.add('booked');
+                  }
+                }
+              }
+            }
+          }
           break;
         }
       }
 
-      // Show dialog based on room availability
+      setState(() {
+        // Update the UI
+      });
+
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -118,7 +137,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
             alignment: Alignment.bottomCenter,
             child: Container(
               margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.50,
+                top: MediaQuery.of(context).size.height * 0.20,
                 left: MediaQuery.of(context).size.width * 0.05,
                 right: MediaQuery.of(context).size.width * 0.05,
                 bottom: MediaQuery.of(context).size.height * 0.07,
@@ -147,38 +166,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                     ),
                   ),
                   const SizedBox(height: 16),
-                  // Check-in and Check-out date entry boxes in a single row
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: checkInDateController,
-                          readOnly: true,
-                          onTap: () =>
-                              _selectDate(context, checkInDateController),
-                          decoration: const InputDecoration(
-                            labelText: 'Check-in Date',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 16),
-                      Expanded(
-                        child: TextField(
-                          controller: checkOutDateController,
-                          readOnly: true,
-                          onTap: () =>
-                              _selectDate(context, checkOutDateController),
-                          decoration: const InputDecoration(
-                            labelText: 'Check-out Date',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 32),
-                  // Booked button with updated styles
+                  // Check button with updated styles
                   SizedBox(
                     width: double.infinity,
                     child: ElevatedButton(
@@ -195,8 +183,29 @@ class _CalendarScreenState extends State<CalendarScreen> {
                         ),
                         elevation: 10,
                       ),
-                      child: const Text('check',
+                      child: const Text('Check',
                           style: TextStyle(color: Colors.white)),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  // Calendar view
+                  TableCalendar(
+                    focusedDay: DateTime.now(),
+                    firstDay: DateTime(2000),
+                    lastDay: DateTime(2100),
+                    calendarFormat: CalendarFormat.month,
+                    eventLoader: (day) {
+                      return _bookedDates[day] ?? [];
+                    },
+                    calendarStyle: CalendarStyle(
+                      todayDecoration: BoxDecoration(
+                        color: Colors.orange,
+                        shape: BoxShape.circle,
+                      ),
+                      markerDecoration: BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
                     ),
                   ),
                 ],
